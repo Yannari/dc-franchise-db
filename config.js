88b1c6f426ad3a5c428a8a3fe731f93fc33f49d5
Yannari <33236948+Yannari@users.noreply.github.com>
2026-01-04 14:56:09 -0500

    update awards page

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Site Configuration - Edit these values to update across ALL pages
 const SITE_CONFIG = {
   seasons: 5,           // Change this number to update everywhere
-  players: 58,          // Change this number to update everywhere
-  currentSeason: 5,     // Current active season
+  players: 68,          // Change this number to update everywhere
+  currentSeason: 6,     // Current active season
   iconFormat: 'name.png' // Icon file format
 };
 

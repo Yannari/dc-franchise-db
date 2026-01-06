d2fae812aa76a6fc581e9e6f30362908f01ec95d
Yannari <33236948+Yannari@users.noreply.github.com>
2026-01-06 14:44:16 -0500

    big upd

---
+++ Backup_Files/README.md
@@ -0,0 +1,1 @@
+# dc-franchise-db
---
+++ Backup_Files/config.js
@@ -0,0 +1,10 @@
+// Site Configuration - Edit these values to update across ALL pages
+const SITE_CONFIG = {
+  seasons: 5,           // Change this number to update everywhere
+  players: 68,          // Change this number to update everywhere
+  currentSeason: 6,     // Current active season
+  iconFormat: 'name.png' // Icon file format
+};
+
+// Don't edit below this line
+window.SITE_CONFIG = SITE_CONFIG;
